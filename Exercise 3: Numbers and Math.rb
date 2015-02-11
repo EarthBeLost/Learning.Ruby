c80a54dcbbbdc0d1ade57110e678bf8ec8bee26b
Daniel Williams <t15@yougothacked.org>
2015-02-11 10:27:23 +0000

    Update Exercise 3: Numbers and Math.rb
    Study Drill 5: Rewrote the program to use floating point numbers, wasn't any issues, worked well.

--- Exercise 3: Numbers and Math.rb
+++ Exercise 3: Numbers and Math.rb
@@ -2,24 +2,24 @@
 puts "I will now cont my chickens:"
 
 # Outputs some text and calculates some maths.
-puts "Hens #{25 + 30 / 6}"
-puts "roosters #{100 - 25 * 3 % 4}"
+puts "Hens #{25.0 + 30.0 / 6.0}"
+puts "roosters #{100.0 - 25.0 * 3.0 % 4.0}"
 
 # Outputs some text.
 puts "Now I will count the eggs:"
 
 # Outputs some maths.
-puts 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+puts 3.0 + 2.0 + 1.0 - 5.0 + 4.0 % 2.0 - 1.0 / 4.0 + 6.0
 
 # Outputs some text.
 puts "Is it true that 3 + 2 < 5 - 7?"
 
 # Outputs some maths.
-puts 3 + 2 < 5 - 7
+puts 3.0 + 2.0 < 5.0 - 7.0
 
 # Outputs some text and calculates some maths.
-puts "What is 3 + 2? #{3 + 2}"
-puts "What is 5 - 7? #{5 - 7}"
+puts "What is 3 + 2? #{3.0 + 2.0}"
+puts "What is 5 - 7? #{5.0 - 7.0}"
 
 # Outputs some text.
 puts "Oh, that's why it's false."
@@ -28,6 +28,6 @@ puts "Oh, that's why it's false."
 puts "How about some more."
 
 # Outputs some text and calculates some maths.
-puts "Is it greater? #{5 > -2}"
-puts "Is it greater or equal? #{5 >= -2}"
-puts "Is it less or equal? #{5 <= -2}"
+puts "Is it greater? #{5.0 > -2.0}"
+puts "Is it greater or equal? #{5.0 >= -2.0}"
+puts "Is it less or equal? #{5.0 <= -2.0}"
