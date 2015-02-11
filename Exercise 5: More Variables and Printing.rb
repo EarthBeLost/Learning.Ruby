2ec2e4ef81df99df6df478a1496c1bc59e38cc98
Daniel Williams <t15@yougothacked.org>
2015-02-11 11:05:26 +0000

    Update Exercise 5: More Variables and Printing.rb
    Study Drill 1c: Finished it, works fine. All good. Added in the weight conversion to KG as well.

--- Exercise 5: More Variables and Printing.rb
+++ Exercise 5: More Variables and Printing.rb
@@ -3,6 +3,7 @@ age = 35 # not a lie in 2009.
 height = 74 # inches.
 height_in_cm = height * 2.540
 weight = 180.0 # lbs.
+weight_in_kg = weight * 0.454
 eyes = 'Blue'
 teeth = 'White'
 hair = 'Brown'
@@ -11,6 +12,7 @@ puts "Let's talk about #{name}."
 puts "He's #{height} inches tall."
 puts "That means he's #{height_in_cm} cm tall."
 puts "He's #{weight} pounds heavy."
+puts "That means he's #{weight_in_kg} kg heavy."
 puts "Actually that's not too heavy."
 puts "He's got #{eyes} eyes and #{hair} hair."
 puts "His teeth are usually #{teeth} depending on the coffee."
