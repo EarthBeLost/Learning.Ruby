983a877219344d0c4e9b73cec5e0187fd123b486
Daniel Williams <t15@yougothacked.org>
2015-02-11 10:10:37 +0000

    Update Exercise 1: A Good First Program.rb
    Study Drill 2: Only printing out 1 line.

--- Exercise 1: A Good First Program.rb
+++ Exercise 1: A Good First Program.rb
@@ -1,8 +1,1 @@
-puts "Hello World!"
-puts "Hello again"
-puts "I like typing this."
-puts "This is fun."
-puts "Yey! Printing."
-puts "I'd much rather you 'not'."
-puts 'I "said" do not touch this.'
 puts "This is another line that I added."
