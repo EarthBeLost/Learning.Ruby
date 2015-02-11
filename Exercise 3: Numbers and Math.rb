b82df4847ebf42ff2ebc27c4246cb20f2127246b
Daniel Williams <t15@yougothacked.org>
2015-02-11 10:22:55 +0000

    Update Exercise 3: Numbers and Math.rb
    Study Drill 1: Got quite lazy with it but still did the comments, seems to go well, not that there would be any issues here.

--- Exercise 3: Numbers and Math.rb
+++ Exercise 3: Numbers and Math.rb
@@ -1,23 +1,33 @@
+# Outputs some text.
 puts "I will now cont my chickens:"
 
+# Outputs some text and calculates some maths.
 puts "Hens #{25 + 30 / 6}"
 puts "roosters #{100 - 25 * 3 % 4}"
 
+# Outputs some text.
 puts "Now I will count the eggs:"
 
+# Outputs some maths.
 puts 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
 
+# Outputs some text.
 puts "Is it true that 3 + 2 < 5 - 7?"
 
+# Outputs some maths.
 puts 3 + 2 < 5 - 7
 
+# Outputs some text and calculates some maths.
 puts "What is 3 + 2? #{3 + 2}"
 puts "What is 5 - 7? #{5 - 7}"
 
+# Outputs some text.
 puts "Oh, that's why it's false."
 
+# Outputs some text.
 puts "How about some more."
 
+# Outputs some text and calculates some maths.
 puts "Is it greater? #{5 > -2}"
 puts "Is it greater or equal? #{5 >= -2}"
 puts "Is it less or equal? #{5 <= -2}"
