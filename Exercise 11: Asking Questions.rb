94ec5fb466f25b6f6050c8414da76bdd2e7b38a9
Daniel Williams <t15@yougothacked.org>
2015-02-28 18:44:19 +0000

    Update Exercise 11: Asking Questions.rb
    Study Drill 3: Made a new form, basically the same concept as the one before, decided to add in some escape sequences for fun.

--- Exercise 11: Asking Questions.rb
+++ Exercise 11: Asking Questions.rb
@@ -1,8 +1,8 @@
-print "How old are you? "
-age = gets.chomp
-print "How tall are you? "
-height = gets.chomp
-print "How much do you weigh? "
-weight = gets.chomp
+print "What is your name? "
+name = gets.chomp
+print "What year were you born? "
+year = gets.chomp
+print "What is your best friends name? "
+friend = gets.chomp
 
-puts "So, you're #{age} old, #{height} tall and #{weight} heavy."
+puts "So, your name is \"#{name}\"; You were born in the year #{year} and your best friend is called \"#{friend}\", right?"
